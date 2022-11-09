60a4166f672b4242fadc065cfdc997f13f9a75fa
macasalad <87970411+macasalad@users.noreply.github.com>
2022-11-10 00:23:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import shutil
     
 st.markdown("""# Vector Addition Calculator
     hahahahahahahaha lagay ng description""")
-    app_logo = Image.open("app_logo.png")
+app_logo = Image.open("app_logo.png")
 st.set_page_config(
     page_title="Vector Addition Calculator",
     page_icon=app_logo,
