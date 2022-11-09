432f1a47b4e1b528559c793716d90bdbf379b881
macasalad <87970411+macasalad@users.noreply.github.com>
2022-11-10 00:15:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ import shutil
 
 if __name__ == "__main__":
     
+    st.markdown("""# Vector Addition Calculator
+    hahahahahahahaha lagay ng description""")
+    
     app_logo = Image.open("app_logo.png")
     st.set_page_config(
         page_title="Vector Addition Program",
         page_icon=app_logo,
     )
+    
+    with st.expander("Credits", expanded = False):
+        st.markdown("""wahaha credits""")
