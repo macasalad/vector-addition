8be172204c8ad517458b21001a34226f4dcf271a
macasalad <87970411+macasalad@users.noreply.github.com>
2022-11-10 00:11:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from PIL import Image
-import numpy as np
 
 st.markdown("""# Vector Addition Calculator
 hahahahahahahaha lagay ng description""")
+
 app_logo = Image.open("app_logo.png")
 st.set_page_config(
-page_title="Vector Addition Calculator",
-page_icon=app_logo,
+  page_title="Vector Addition Calculator",
+  page_icon=app_logo,
 )
